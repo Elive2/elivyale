8f9941e5219d81b74d29b3cf7485bd44c16fd460
Eli Yale <elivyale@gmail.com>
2020-12-16 09:47:52 -0800

    adds experience grid

---
+++ js/grid.js
@@ -0,0 +1,24 @@
+let grid = document.getElementById("exp-grid");
+
+function plusClicked(GridItemID) {
+	let element = document.getElementById(GridItemID);
+
+	let body = element.querySelector('.grid-item-body');
+	let plusButton = element.querySelector('.plus');
+	let secondSpan = plusButton.childNodes[1];
+	if (body.classList.contains("grid-item-active")) {
+		body.classList.remove("grid-item-active");
+		secondSpan.classList.remove("button-clicked");
+		secondSpan.style.left = "25%";
+		secondSpan.style.right = "25%";
+		plusButton.style.webkitTransform = "rotate(180deg)";
+
+	}
+	else {
+		body.classList.add("grid-item-active");
+		secondSpan.style.left = "50%";
+		secondSpan.style.right = "50%";
+		plusButton.style.webkitTransform = "rotate(90deg)";
+
+	}
+};
